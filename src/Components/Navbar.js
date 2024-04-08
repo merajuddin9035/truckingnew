10aa85943327be8086df4990a794f20df2673635
mdmerajuddin <mohammedmerajuddin9035@gmail.com>
2024-04-08 15:39:16 +0530

    dddd

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,32 +9,38 @@ export const Navbar = () => {
     setShowMenu(!showMenu);
   };
 
+  // Function to close the menu
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
     <nav className="navbar">
       <h1 className="Advaitheading">ADVAIT TRUCKING AUTOMATION</h1>
-      <div className="navbar-container">
-        <div className="menu" style={{ display: showMenu ? "block" : "none" }}>
-          <ul>
-            <li className="active">
-              <Link to="/" onClick={toggleMenu}>
+      <div className={`navbar-container ${showMenu ? "active" : ""}`}>
+        <div className="menu">
+          <ul className={showMenu ? "active" : ""}>
+            <li>
+              <Link to="/" onClick={closeMenu}> {/* Close menu when a link is clicked */}
                 Home
               </Link>
             </li>
             <li>
-              <Link to="/finance" onClick={toggleMenu}>
+              <Link to="/finance" onClick={closeMenu}>
                 Finance Application
               </Link>
             </li>
             <li>
-              <Link to="/about" onClick={toggleMenu}>
-                About us
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" onClick={toggleMenu}>
+              <Link to="/contact" onClick={closeMenu}>
                 Contact us
               </Link>
             </li>
+            <li>
+              <Link to="/about" onClick={closeMenu}>
+                About us
+              </Link>
+            </li>
+           
           </ul>
         </div>
         <div className="toggle-menu" onClick={toggleMenu}>

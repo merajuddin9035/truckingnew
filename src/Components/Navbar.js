ed2387c2c1b615ddaf89a99a13f03eca7f213114
mdmerajuddin <mohammedmerajuddin9035@gmail.com>
2024-04-10 14:32:45 +0530

    chnagesd

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,7 +19,7 @@ export const Navbar = () => {
       <ul className={`links ${isOpen ? 'open' : ''}`}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/finance">Finance</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
       
--- src/Contact.js
+++ src/Contact.js
@@ -1,64 +1,13 @@
-import React,{useState} from 'react';
+import React from 'react';
 import './Contact.css';
-import contactus from "./Assets/contactus.png"
+import contactusframesss from './Assets/contactusframesss.png'
+
 
 const Contact = () => {
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission
-    console.log("Form submitted!");
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Message:", message);
-    // Reset form fields after submission
-    setName("");
-    setEmail("");
-    setMessage("");
-  }
   return (
     <>
-  <div className='contactus-container'> 
-        <img className="contactus" src={contactus} alt="Truck" />
-        <div className="form-container">
-        <h2>Contact Us</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-            ></textarea>
-          </div>
-          <button className='contactusbutton' type="submit">Submit</button>
-        </form>
-      </div>
+   <div className='contactus-container'> 
+        <img className="contactus" src={contactusframesss} alt="contactus" />
         </div>
      
     </>
